620ce6a06d13a3a72dea14799cc9d602876f981e
Mathew, Bibi <bibi.mathew@pnnl.gov>
2025-04-03 13:25:43 -0500

    adding mantine related configs and labraries and first cut for header and homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+import { Title } from '@mantine/core';
+
+export const HomePage = () => {
+    return (
+        <Title order={1}>Many-Body Methods, Spectroscopies, and Dynamics for Molecular Polaritonic SystemsPermalink</Title>
+    )
+}
---
+++ src/pages/TeamsPage.jsx
@@ -0,0 +1,8 @@
+import { Title } from '@mantine/core';
+
+export const TeamsPage  = () => {
+    return (
+        <Title order={1}>Team</Title>
+        
+    )
+}   
