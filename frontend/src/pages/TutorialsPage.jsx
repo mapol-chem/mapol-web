6ca6f562d13fcd2fbcfb03cb488a3e53a21b5c4a
Ajay Panyala <ajay.panyala@gmail.com>
2025-07-23 15:55:49 -0700

    add python notebooks

--- frontend/src/pages/TutorialsPage.jsx
+++ frontend/src/pages/TutorialsPage.jsx
@@ -5,7 +5,9 @@ export const TutorialsPage = () => {
     <Container fluid>
       <Title order={1}>Tutorials</Title>
       <Space h="xl" />
-      <Anchor href="https://mybinder.org/v2/gh/mapol-chem/psi4polaritonic/live_nb" target="_blank">Jupyter Notebooks</Anchor>
+      <Anchor href="https://mybinder.org/v2/gh/mapol-chem/mapol-web/HEAD?urlpath=%2Fdoc%2Ftree%2Fnotebooks%2FQED-CIS-1.ipynb" target="_blank">QED-CIS Python Notebook</Anchor>
+      <Space h="sm" />
+      <Anchor href="https://mybinder.org/v2/gh/mapol-chem/mapol-web/HEAD?urlpath=%2Fdoc%2Ftree%2Fnotebooks%2FQED-HF_Tutorial.ipynb" target="_blank">QED-HF Tutorial Python Notebook</Anchor>
        </Container>
     )
 }
