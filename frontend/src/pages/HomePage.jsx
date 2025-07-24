144d0bc7820fee500891bed58d3156366f6b44d0
Mathew, Bibi <bibi.mathew@pnnl.gov>
2025-07-24 12:28:54 -0500

    fixing image size and alignement on home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,8 +9,9 @@ import {
   Grid,
   Space,
   Divider,
-} from '@mantine/core';
-import mapol_graphic from '../images/mapol.jpeg';
+  Box,
+} from '@mantine/core'
+import mapol_graphic from '../images/mapol.jpeg'
 export const HomePage = () => {
   return (
     <>
@@ -20,41 +21,51 @@ export const HomePage = () => {
           Polaritonic Systems
         </Title>
 
-        <Grid align="center">
-          <Grid.Col span={8}>
-            <Text align="justify" fw={500}>
-              Light when nanoconfined in cavities and made to interact strongly
-              with molecules results in hybrid light–matter states or
-              polaritons, which can be used to modify and manipulate ground and
-              excited-state potential energy surfaces and thus alter the
-              reactivity and energy flow profoundly in molecular systems.The
-              development of predictive theories for molecular polaritons
-              requires combining electronic degrees of freedom, as provided by
-              quantum chemistry methods, with quantum electrodynamics (QED)
-              treatments of the photonic degrees of freedom.
-            </Text>
-            <Space h="sm" />
-            <Text fw={500} align="justify">
-              Our vision is to develop a suite of theoretical frameworks,
-              spanning low-order to high-level many-body methods, that will
-              provide unprecedented ability to capture the structure and
-              dynamics of molecular polaritonic systems and deliver scalable
-              software source code and libraries that can harness
-              leadership-class computational facilities (LCFs). In pursuit of
-              this vision, we have three project objectives to guide our
-              research program, which was initiated in September 2022.
-            </Text>
-          </Grid.Col>
-          <Grid.Col span={4}>
-            <Image src={mapol_graphic} alt="Mapol Graphic" />
-          </Grid.Col>
-        </Grid>
+        {/* <Grid align="center"> */}
+        <Box pt="lg">
+          <Grid justify="center" align="center">
+            <Grid.Col span={8}>
+              <Text align="justify" fw={500}>
+                Light when nanoconfined in cavities and made to interact
+                strongly with molecules results in hybrid light–matter states or
+                polaritons, which can be used to modify and manipulate ground
+                and excited-state potential energy surfaces and thus alter the
+                reactivity and energy flow profoundly in molecular systems.The
+                development of predictive theories for molecular polaritons
+                requires combining electronic degrees of freedom, as provided by
+                quantum chemistry methods, with quantum electrodynamics (QED)
+                treatments of the photonic degrees of freedom.
+              </Text>
+              <Space h="sm" />
+              <Text fw={500} align="justify">
+                Our vision is to develop a suite of theoretical frameworks,
+                spanning low-order to high-level many-body methods, that will
+                provide unprecedented ability to capture the structure and
+                dynamics of molecular polaritonic systems and deliver scalable
+                software source code and libraries that can harness
+                leadership-class computational facilities (LCFs). In pursuit of
+                this vision, we have three project objectives to guide our
+                research program, which was initiated in September 2022.
+              </Text>
+            </Grid.Col>
+            <Grid.Col span="auto">
+              <Image
+                src={mapol_graphic}
+                fit="contain"
+                width="100%"
+                height="auto"
+                style={{ maxHeight: '250px' }}
+                alt="Mapol Graphic"
+              />
+            </Grid.Col>
+          </Grid>
+        </Box>
         <br />
         <Title align="left" order={2}>
           Objectives
         </Title>
         <Divider my="md" />
-        <List type="ordered" spacing="md"> 
+        <List type="ordered" spacing="md">
           <List.Item>
             <Text size="md" align="left">
               Develop a Gaussian basis set–based suite of methods spanning
@@ -97,5 +108,5 @@ export const HomePage = () => {
         </Text>
       </Container>
     </>
-  );
-};
+  )
+}
